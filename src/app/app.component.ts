954a422393fa2d763bb6bbf69b1bffead79cd1f9
Diogo Camilo <diogo.santos@interplayers.com.br>
2023-01-12 17:51:37 -0300

    Initial configuration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-
-
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private sqlite: SQLite) { 
+    this.query();
+  }
 
+  
+  query(): any {
+    this.sqlite.create({
+      name: 'marketplace-mobile.db',
+      location: 'default'
+    })
+      .then((db: SQLiteObject) => {
+
+
+        db.executeSql('create table danceMoves(name VARCHAR(32))', [])
+          .then(() => console.log('Executed SQL'))
+          .catch(e => console.log(e));
+      })
+      .catch(e => console.log(e));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
     IonicModule.forRoot(), 
     AppRoutingModule
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [SQLite, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
